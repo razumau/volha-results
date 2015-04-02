98ecc2efc2e60fbcc156fe50035ec94919f3a2f4
Jury Razumau <jurazumau@gmail.com>
2015-04-03 02:03:55 +0300

    move to production port

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.get('/:key', function(req, res) {
 
 })
 
-var server = app.listen(3000, function() {
+var server = app.listen(80, function() {
 
 	var host = server.address().address
 	var port = server.address().port
